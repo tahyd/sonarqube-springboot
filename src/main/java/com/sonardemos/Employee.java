a82e9333e372ccc0e59c53686b104ea0772273c6
tahyd <ta.hyd.003@gmail.com>
2025-02-09 14:18:10 +0530

    sonar properties added6

--- src/main/java/com/sonardemos/Employee.java
+++ src/main/java/com/sonardemos/Employee.java
@@ -3,4 +3,11 @@ package com.sonardemos;
 public class Employee {
     private int id;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
