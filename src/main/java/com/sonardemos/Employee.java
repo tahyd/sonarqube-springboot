69dbac87b61ddfe1313ca60e413fa4910c88e2a6
tahyd <140394321+tahyd@users.noreply.github.com>
2025-02-10 13:17:51 +0530

    Update Employee.java

--- src/main/java/com/sonardemos/Employee.java
+++ src/main/java/com/sonardemos/Employee.java
@@ -1,13 +1,5 @@
 package com.sonardemos;
 
 public class Employee {
-    private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+  
 }
